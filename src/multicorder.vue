3bf7b2c3b107467ba73bf13f1fee54891969275c
Brian Winkers <bwinkers@gmail.com>
2021-04-19 19:50:28 -0700

    Vuetify working

--- src/multicorder.vue
+++ src/multicorder.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Recorder goes here</div>
+  <div>
+    Recorder goes here <v-btn outlined rounded text> Button </v-btn>
+    <v-card class="mx-auto" max-width="344" outlined>
+      Hello
+      <v-card-actions>
+        <v-btn outlined rounded text> Button </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
+
+<script>
+import { VBtn } from "vuetify/lib";
+import vuetify from "./plugins/vuetify";
+
+export default {
+  name: "Multicorder",
+  vuetify,
+  components: {
+    VBtn,
+  },
+};
+</script>
