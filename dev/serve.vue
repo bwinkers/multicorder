41f8e246df06ee9ab4306fa9cc65788475212a22
Brian Winkers <bwinkers@gmail.com>
2021-04-24 10:54:39 -0700

    Initial library project from vue-sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MulticorderSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  MulticorderSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <multicorder-sample />
+  </div>
+</template>
